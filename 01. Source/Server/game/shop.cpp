7e0fa1ac050eef17d2bb830ba6f6118e68a1a223
WLsj24 <55287366+WLsj24@users.noreply.github.com>
2019-09-27 20:52:58 +0200

    Update shop.cpp
    Added:
    + ENABLE_OFFLINE_SHOP_USE_CHEQUE
    + ENABLE_SHOP_USE_CHEQUE
    
    This allows buy and sell in normal_shop(pc-poly) and offline shop (by great)

--- 01. Source/Server/game/shop.cpp
+++ 01. Source/Server/game/shop.cpp
@@ -40,7 +40,7 @@ Add after:
 
 
 /*--------------------------------------------------*/
-//Step 2
+//Step 2	For pc_shop
 /*--------------------------------------------------*/
 /*Search in:
 int CShop::Buy(LPCHARACTER ch, BYTE pos)
@@ -120,5 +120,149 @@ Add after:
 */
 #ifdef ENABLE_SHOP_USE_CHEQUE
 	ch->PointChange(POINT_CHEQUE, -dwCheque, false);
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+		m_pkPC->PointChange(POINT_GOLD, dwPrice, false);
+
+Add after:
+*/
+#ifdef ENABLE_SHOP_USE_CHEQUE
+		m_pkPC->PointChange(POINT_CHEQUE, dwCheque, false);
+#endif
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+//Step 3	For offline_shop (by great)
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+	if (r_item.price < 0)
+	{
+		LogManager::instance().HackLog("SHOP_BUY_GOLD_OVERFLOW", ch);
+		return SHOP_SUBHEADER_GC_NOT_ENOUGH_MONEY;
+	}
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+	if (r_item.cheque<0)
+	{
+		LogManager::instance().HackLog("SHOP_BUY_CHEQUE_OVERFLOW", ch);
+		return SHOP_SUBHEADER_GC_NOT_ENOUGH_CHEQUE;
+	}
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+	DWORD dwPrice = r_item.price;
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+	DWORD dwCheque = r_item.cheque;
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+	if (ch->GetGold() < r_item.price)
+	{
+		sys_log(1, "Shop::Buy : Not enough money : %s has %d, price %d", ch->GetName(), ch->GetGold(), dwPrice);
+		return SHOP_SUBHEADER_GC_NOT_ENOUGH_MONEY;
+	}
+
+Replace with:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+	if ((int)dwPrice>0 && (int)dwCheque > 0) // Yang-Cheque
+	{
+		if (ch->GetGold() < (int)dwPrice || ch->GetCheque() < (int)dwCheque)
+			return SHOP_SUBHEADER_GC_NOT_ENOUGH_MONEY_CHEQUE;
+	}
+	else if ((int)dwPrice>0 && (int)dwCheque <= 0) // Yang
+	{
+		if (ch->GetGold() < (int)dwPrice)
+			return SHOP_SUBHEADER_GC_NOT_ENOUGH_MONEY;
+	}
+	else if ((int)dwPrice<=0 && (int)dwCheque > 0) // cheque
+	{
+		if (ch->GetCheque() < (int)dwCheque)
+			return SHOP_SUBHEADER_GC_NOT_ENOUGH_CHEQUE;
+	}
+#else
+	if (ch->GetGold() < r_item.price)
+	{
+		sys_log(1, "Shop::Buy : Not enough money : %s has %d, price %d", ch->GetName(), ch->GetGold(), dwPrice);
+		return SHOP_SUBHEADER_GC_NOT_ENOUGH_MONEY;
+	}
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+	ch->PointChange(POINT_GOLD, -dwPrice, false);
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+	ch->PointChange(POINT_CHEQUE, -dwCheque, false);
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+				DBManager::instance().DirectQuery("UPDATE player_shop SET gold=gold + %d WHERE id=%d", dwPrice, m_pkPC->GetPrivShop());
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+				DBManager::instance().DirectQuery("UPDATE player_shop SET cheque=cheque + %d WHERE id=%d", dwCheque, m_pkPC->GetPrivShop());
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+int CShop::Buy(LPCHARACTER ch, BYTE pos)
+
+This:
+				m_pkPC->PointChange(POINT_GOLD, dwPrice, false);
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+				m_pkPC->PointChange(POINT_CHEQUE, dwCheque, false);
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+void CShop::SetPrivShopItems(std::vector<TShopItemTable *> map_shop)
+
+This:
+		item.itemid = 0;
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+		item.cheque = 0;
+#endif
+/*--------------------------------------------------*/
+/*Search in:
+void CShop::SetPrivShopItems(std::vector<TShopItemTable *> map_shop)
+
+This:
+			item.itemid = pkItem->GetRealID();
+
+Add after:
+*/
+#ifdef ENABLE_OFFLINE_SHOP_USE_CHEQUE
+			item.cheque = pTable->cheque;
 #endif
 /*--------------------------------------------------*/
